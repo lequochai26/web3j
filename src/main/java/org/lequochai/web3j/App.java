42b00fdd1587debc888654c71e153a894489d63c
Lê Quốc Hải <lequochai26@gmail.com>
2023-10-27 17:04:00 +0700

    Project initialization

---
+++ src/main/java/org/lequochai/web3j/App.java
@@ -0,0 +1,13 @@
+package org.lequochai.web3j;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
